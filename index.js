5e552e796515e42333341e986b77eb036bc00be9
satyasanatanbastia <satyasanatanbastia@gmail.com>
2024-09-22 21:10:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// console.log("Sri radha rani");
+// console.log("Sri radha");
+ 
+
+// // variable
+
+// // use let
+// if(true){
+//     let a= 4
+//     console.log(a);
+// }
+// // console.log(a)
+
+// // use var
+
+// var x=6;
+// if (true) {
+//     console.log(x);
+// }
+
+// console.log("Sri radha");
+
+// let aa =null
+// let bb 
+// console.log(aa)
+// console.log(bb)
+
+
+// let a = 5
+// console.log(a)
+// a = "Satya"
+// console.log(a)
+// a= true
+// console.log(a)
+
+let a= 5
+console.log(a++)
+console.log(a)
+
+console.log("------------------")
+let x= 10
+console.log(x)
+console.log(++x)
+console.log(x)
+
+console.log("------------------")
+let y= 20
+console.log(y)
+console.log(--y)
+console.log(y)
+
+console.log("------------------")
+
+let z= 30
+console.log(z)
+console.log(z--)
+console.log(z)
