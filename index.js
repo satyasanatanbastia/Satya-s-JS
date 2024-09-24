a5c63c47c9d95a329923fc879b94fa029a74e671
Satya Sanatan Bastia <satyasanatanbastia@gmail.com>
2024-09-24 22:45:11 +0530

    new change

--- index.js
+++ index.js
@@ -33,25 +33,93 @@
 // a= true
 // console.log(a)
 
-let a= 5
-console.log(a++)
-console.log(a)
+// let a= 5
+// console.log(a++)
+// console.log(a)
 
-console.log("------------------")
-let x= 10
-console.log(x)
-console.log(++x)
-console.log(x)
+// console.log("------------------")
+// let x= 10
+// console.log(x)
+// console.log(++x)
+// console.log(x)
 
-console.log("------------------")
-let y= 20
-console.log(y)
-console.log(--y)
-console.log(y)
+// console.log("------------------")
+// let y= 20
+// console.log(y)
+// console.log(--y)
+// console.log(y)
 
-console.log("------------------")
+// console.log("------------------")
 
-let z= 30
-console.log(z)
-console.log(z--)
-console.log(z)
+// let z= 30
+// console.log(z)
+// console.log(z--)
+// console.log(z)
+
+
+//ternary operator
+
+// let age = 22
+// let check  = (age>=18) ? "yes u can vote" : "sorry u not eligible for vote";
+// console.log(check)
+
+
+// console.log(false || "satya")
+
+
+//control statements:-
+
+// let age = 4
+// if (age>=60) {
+//     console.log("U R Retaired")
+// }else if (age>=35) {
+//     console.log("U R Experienced Working Proffesional")
+// }else{
+//     console.log("you are Young Proffesional")
+// }
+
+
+//switch case
+
+// let day=1;
+// switch (day) {
+//     case 1: console.log("Sunday");
+//     break;
+//     case 2: console.log("Monday");
+//     break;
+//     case 3: console.log("Tuesday");
+//     break;
+//     case 4: console.log("Wednesday");
+//     break;
+//     case 5: console.log("Thursday");
+//     break;
+//     case 6: console.log("friday");
+//     break;
+//     case 7: console.log("Saturday");
+//     break;
+    
+//     default: console.log("sorry")
+//     break;
+// }
+
+//looping in js
+
+// for (let i = 0; i<=8 ; i++) {
+//     console.log(i +"satya");
+// }
+
+ 
+// let i=0;
+// while (i<=32) {
+//     console.log(i);
+//     i++;
+// }
+
+// let  i= 0;
+
+// do {
+//   console.log(i);
+//   i++; 
+// } while (i <= 5);
+
+
