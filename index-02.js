143cdb95c9fe7205062ba9d93994de2ec2e9d620
Satya Sanatan Bastia <satyasanatanabastia@gmail.com>
2024-12-15 00:25:44 +0530

    commit new

--- index-02.js
+++ index-02.js
@@ -5,3 +5,15 @@ console.log("radhe radhe")
 
 // JS Object 
 
+console.log(typeof null)
+//VM156:1 object
+
+console.log(typeof undefined)
+//VM189:1 undefined
+
+score="satya"
+console.log(typeof score)//string
+console.log(typeof (score))//string
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)//number
+console.log(valueInNumber)//Nan  ---> nan it is a type  
