a35468fc555cc7cd8a9cbc8a7f50850c78e6dd63
Satya Sanatan Bastia <satyasanatanabastia@gmail.com>
2025-01-01 21:18:24 +0530

    new commit

--- index-02.js
+++ index-02.js
@@ -16,4 +16,14 @@ console.log(typeof score)//string
 console.log(typeof (score))//string
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)//number
-console.log(valueInNumber)//Nan  ---> nan it is a type  
+console.log(valueInNumber)//Nan  ---> nan it is a type      
+
+
+// "33" =>33 converted easily
+
+// "33avb" =>NaN, but type off "33avb" is number
+
+const satya="33abc"
+console.log(typeof (satya))
+const satya_1 = Number(satya)
+console.log(typeof (satya_1))
---
+++ index3.js
@@ -0,0 +1,39 @@
+//Datatype conversion
+
+// let age =33
+// console.log(typeof age)//number
+// console.log(typeof(age))//number
+
+// let ab= "23"
+// let c = Number(ab)
+// console.log(typeof(c))//number
+
+// let a= "23abc"
+// let valueInNumber = Number(a)
+// console.log(typeof(valueInNumber))//number
+// console.log(valueInNumber)//NaN
+
+
+// let isLoggedIn = ""
+// console.log(isLoggedIn)
+// let a1 = Boolean(isLoggedIn)
+// console.log(a1)//false
+
+// let isLoggedIn = "0"
+// console.log(isLoggedIn)
+// let a1 = Boolean(isLoggedIn)
+// console.log(a1)//true
+
+// let isLoggedIn = "1"
+// console.log(isLoggedIn)
+// let a1 = Boolean(isLoggedIn)
+// console.log(a1)//true
+
+// ****************operations***************
+
+
+console.log("1"+2)//12
+console.log(1+'2')//12
+console.log("1"+"2")//12
+console.log("1"+2 +3)//123
+console.log(1 +2 +'3')//33
