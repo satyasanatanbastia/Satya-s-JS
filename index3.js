cd332d6c7e33033d50183d32b5727ca7757135a2
Satya Sanatan Bastia <satyasanatanabastia@gmail.com>
2025-01-20 00:19:04 +0530

    new commit

--- index3.js
+++ index3.js
@@ -95,6 +95,36 @@
 
 // Strings in Javascript
 
-const name="satya"
-const age=43
-console.log(`my name is ${name} and my age is ${age}`)
+// const name="satya"
+// const age=43
+// console.log(`my name is ${name} and my age is ${age}`)
+
+// another way to declare String
+const myName = new String('Satya sanatan bastia')
+console.log(myName)
+console.log(myName[4])
+console.log(myName.__proto__)
+console.log(myName.length)
+console.log(myName.toLocaleLowerCase())//convert all in lower Case
+console.log(myName.toLocaleUpperCase())//convert all in Upper Case
+console.log(myName.charAt(3))//provide Which charecter present in Pass Nuber Location
+console.log(myName.indexOf('a'))//provide details of passing charecter present in which location
+
+const newString = myName.substring(0,5)
+console.log(newString)// substring
+
+const cfc =myName.slice(2, 6)
+console.log(cfc)// use slice of main string and store it in new string
+
+const bff = "          satya     "
+console.log(bff)
+console.log(bff.trim())// remove the string of string
+
+const nmn =" satya sanatan sonu"
+console.log(nmn.includes('sonu'))// check the passing string is available or not in main string
+console.log(nmn.split('-',1))//split is use to there separator(special delimiter) to check the entire string and devide them in substring if not found any separtor then print entire string in array format
+
+
+
+
+
