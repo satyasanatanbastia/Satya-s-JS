1db6b20b464c2aeb9d31e827306ea213c9d8cd73
Satya Sanatan Bastia <satyasanatanabastia@gmail.com>
2025-01-23 15:12:31 +0530

    Your commit message here

---
+++ array.js
@@ -0,0 +1,40 @@
+//************ARRAY IN JS**********
+// normal creation of an array
+// const myArray =[1,'satya', true , 0, 68.08]
+// console.log(typeof(myArray))
+// console.log(`this is my first array ${myArray}`)
+
+//create array using array object
+// const vdp= new Array('mom', 44, 87, "gcp")
+// console.log(vdp)
+// console.log(typeof(vdp))
+// //accessing an array
+// console.log(vdp[2])// index start from 0 ,1st item is 0th index
+
+//array methods
+
+const nnnt = [1,2,4,78,6,1,6,8]
+// nnnt.push(33)
+// console.log(nnnt)
+// nnnt.pop()
+// console.log(nnnt)
+
+// push():
+// Adds an element to the end of the array.
+// Directly modifies the original array (nnnt).
+// Returns the new length of the array (but this return value is not used in your code).
+
+
+// pop():
+// Removes the last element from the array.
+// Directly modifies the original array (nnnt).
+// Returns the removed element (but again, this return value is not used in your code).
+//nnnt.unshift("ram")//this is not best practice to do like this rare cases it is used . if we add any items in 0 index all items index is changed it take more time taking and pressure for large file
+
+//nnnt.shift("ram") //its removes 1st elemet like pop
+
+//console.log(nnnt.includes(121))//Checks if an array contains a specific value.
+
+//console.log(nnnt.indexOf(6))// passing the value to check present  in which index and provide the index number, if the passing value is not found then print -1
+
+
--- index3.js
+++ index3.js
@@ -100,31 +100,85 @@
 // console.log(`my name is ${name} and my age is ${age}`)
 
 // another way to declare String
-const myName = new String('Satya sanatan bastia')
-console.log(myName)
-console.log(myName[4])
-console.log(myName.__proto__)
-console.log(myName.length)
-console.log(myName.toLocaleLowerCase())//convert all in lower Case
-console.log(myName.toLocaleUpperCase())//convert all in Upper Case
-console.log(myName.charAt(3))//provide Which charecter present in Pass Nuber Location
-console.log(myName.indexOf('a'))//provide details of passing charecter present in which location
+// const myName = new String('Satya sanatan bastia')
+// console.log(myName)
+// console.log(myName[4])
+// console.log(myName.__proto__)
+// console.log(myName.length)
+// console.log(myName.toLocaleLowerCase())//convert all in lower Case
+// console.log(myName.toLocaleUpperCase())//convert all in Upper Case
+// console.log(myName.charAt(3))//provide Which charecter present in Pass Nuber Location
+// console.log(myName.indexOf('a'))//provide details of passing charecter present in which location
 
-const newString = myName.substring(0,5)
-console.log(newString)// substring
+// const newString = myName.substring(0,5)
+// console.log(newString)// substring
 
-const cfc =myName.slice(2, 6)
-console.log(cfc)// use slice of main string and store it in new string
+// const cfc =myName.slice(2, 6)
+// console.log(cfc)// use slice of main string and store it in new string
 
-const bff = "          satya     "
-console.log(bff)
-console.log(bff.trim())// remove the string of string
+// const bff = "          satya     "
+// console.log(bff)
+// console.log(bff.trim())// remove the string of string
 
-const nmn =" satya sanatan sonu"
-console.log(nmn.includes('sonu'))// check the passing string is available or not in main string
-console.log(nmn.split('-',1))//split is use to there separator(special delimiter) to check the entire string and devide them in substring if not found any separtor then print entire string in array format
-
-
+// const nmn =" satya sanatan sonu"
+// console.log(nmn.includes('sonu'))// check the passing string is available or not in main string
+// console.log(nmn.split('-',1))//split is use to there separator(special delimiter) to check the entire string and devide them in substring if not found any separtor then print entire string in array format
 
 
+//Number and Maths in Javascript 
+// const score = 2000
+// console.log (score)//normal declaration of number 
 
+// const balance = new Number(1475)
+// console.log(balance) 
+// console.log(balance.toString()) //to string convert entire data type to string format
+
+// console.log(balance.toString().length)// length is use how many charecter present in string
+
+// console.log(balance.toFixed(1)); // toFixed & it is accept under there parenthesis called argument number is used to format a number to a fixed number of decimal places. It returns the number as a string, rounded to the specified number of decimals.
+
+// //it also work like this
+// let num = 123.456;
+
+// // Round to 2 decimal places
+// console.log(num.toFixed(2)); // "123.46"
+
+// // Round to 0 decimal places  
+// console.log(num.toFixed(0)); // "123"
+
+
+//math Object
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(9.5))
+// console.log(Math.ceil(3.8))
+// console.log(Math.floor(4.9))
+// console.log(Math.random()*10)
+// Function to generate a random OTP of a given length
+// function generateOTP(length) {
+//     const characters = '0123456789'; // You can add letters if needed (e.g., 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+//     let otp = '';
+
+//     for (let i = 0; i < length; i++) {
+//         const randomIndex = Math.floor(Math.random() * characters.length);
+//         otp += characters[randomIndex];
+//     }
+
+//     return otp;
+// }
+
+// // Example usage
+// const otpLength = 6; // Set the desired length of the OTP
+// const otp = generateOTP(otpLength);
+// console.log(`Your OTP is: ${otp}`);
+
+
+//Date in JS
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(`toString--- ${myDate.toString()}`);
+// console.log(`toISOString--- ${myDate.toISOString()}`);
+// console.log(`toJSON--- ${myDate.toJSON()}`);
+// console.log(`toLocaleDateString--- ${myDate.toLocaleDateString()}`);
+// console.log(`toDateString--- ${myDate.toDateString()}`);
