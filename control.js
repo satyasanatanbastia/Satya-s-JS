03506f9a9be6d1a317585b9da1418b033c3e0e10
Satya Sanatan Bastia <satyasanatanabastia@gmail.com>
2025-03-11 21:31:19 +0530

    new commit

--- control.js
+++ control.js
@@ -45,12 +45,48 @@
 
 //best practice write cide use scope
 
-// else-if
-const balance=4800
-if (balance>=5000){
-    console.log(`you get 10% Discount on ${balance}`);
-}else if(balance>=3000 && balance<=5000) {
-    console.log(`you get 7% Discount on ${balance}`);
-}else {
-    console.log(`you get 3% Discount on ${balance}`);
-}
+// // else-if
+// const balance=4800
+// if (balance>=5000){
+//     console.log(`you get 10% Discount on ${balance}`);
+// }else if(balance>=3000 && balance<=5000) {
+//     console.log(`you get 7% Discount on ${balance}`);
+// }else {
+//     console.log(`you get 3% Discount on ${balance}`);
+// }
+
+
+// switch case 
+// syntax
+// switch (expression) {
+//     case value1:
+//       // Code chalega agar expression === value1
+//       break;
+//     case value2:
+//       // Code chalega agar expression === value2
+//       break;
+//     default:
+//       // Ye chalega agar koi bhi case match nahi hua
+//   }
+  
+
+// let fruit = "Apple";
+
+// switch (fruit) {
+//   case "Mango":
+//     console.log("It's Mango");
+//     break;
+//   case "Apple":
+//     console.log("It's Apple");
+//     break;
+//   default:
+//     console.log("Unknown Fruit");
+// }
+
+// falsy values 
+// false ,0, -0, "", null, undefined, NaN	Not a Number 
+
+// tuthy values
+
+// "hello", 42, -42, " ", function() {}, {}, []
+
